0c3619c140f250dfb2dd737b56571962f61a69d2
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-12-02 18:40:04 +0300

    Add one more test🧪

--- indexer/src/services/pagemeta-parser/content-parser/ContentParser.test.ts
+++ indexer/src/services/pagemeta-parser/content-parser/ContentParser.test.ts
@@ -90,3 +90,25 @@ Deno.test(
     );
   }
 );
+
+Deno.test(
+  {
+    name: "Test ContentParser - Мы уже мертвы",
+    permissions: { read: true, write: true },
+  },
+  async () => {
+    const rawHTML = await new FileReader(import.meta).getFileContents(
+      "./Мы_уже_мертвы.source.html"
+    );
+    const parsedContent = await new ContentParser(
+      new DOMParser(),
+      new HTMLProcessor(new DOMParser(), {
+        originURL: "http://kriper.net",
+      })
+    ).parse(rawHTML);
+    await new Snapshot(import.meta).snapshotCheck(
+      parsedContent,
+      "./Мы_уже_мертвы.snapshot.md"
+    );
+  }
+);
