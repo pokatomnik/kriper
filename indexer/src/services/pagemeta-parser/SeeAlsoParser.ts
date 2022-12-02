3457fd74ac9c755d691ebba6a9709e45e2d214c6
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-12-02 12:39:07 +0300

    add see also mock implementation

---
+++ indexer/src/services/pagemeta-parser/SeeAlsoParser.ts
@@ -0,0 +1,14 @@
+import type { IParser } from "../lib/IParser.ts";
+import { provide } from "provide";
+
+/**
+ * Mock implementation
+ * TODO wait for template changes
+ */
+export class SeeAlsoParser implements IParser<Array<string>> {
+  public parse(_: string): Promise<Array<string>> {
+    return Promise.resolve([]);
+  }
+}
+
+provide(SeeAlsoParser, []);
