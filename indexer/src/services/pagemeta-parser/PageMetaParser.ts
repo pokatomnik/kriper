3945746c80f748c8887eacf65afc45be2906cf42
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-12-04 22:31:13 +0300

    Inject all deps in PageMetaParser

--- indexer/src/services/pagemeta-parser/PageMetaParser.ts
+++ indexer/src/services/pagemeta-parser/PageMetaParser.ts
@@ -3,6 +3,21 @@ import type { IParser } from "../lib/IParser.ts";
 import type { IHasher } from "../lib/IHasher.ts";
 import type { IAsyncStorage } from "../lib/IAsyncStorage.ts";
 import type { IUncheckedDate } from "../../domain/IUncheckedDate.ts";
+import { provide } from "provide";
+import { NameHasher } from "../name-hasher/NameHasher.ts";
+import { TitleParser } from "./title-parser/TitleParser.ts";
+import { ContentParser } from "./content-parser/ContentParser.ts";
+import { AuthorNicknameParser } from "./author-nickname-parser/AuthorNicknameParser.ts";
+import { DateCreatedParser } from "./date-created-parser/DateCreatedParser.ts";
+import { NumberOfViewsParser } from "./number-of-views-parser/NumberOfViewsParser.ts";
+import { ReadingTimeMinutesParser } from "./reading-time-minutes-parser/ReadingTimeMinutesParser.ts";
+import { SourceParser } from "./source-parser/SourceParser.ts";
+import { RatingParser } from "./rating-parser/RatingParser.ts";
+import { TagsParser } from "./tags-parser/TagsParser.ts";
+import { SeeAlsoParser } from "./see-also-parser/SeeAlsoParser.ts";
+import { ImagesParser } from "./images-parser/ImagesParser.ts";
+import { VideosParser } from "./videos-parser/VideosParser.ts";
+import { ContentSaver } from "../storage/ContentSaver.ts";
 
 export class PageMetaParser implements IParser<IPageMeta> {
   public constructor(
@@ -18,7 +33,7 @@ export class PageMetaParser implements IParser<IPageMeta> {
     private readonly tagsParser: IParser<ReadonlyArray<string>>,
     private readonly seeAlsoParser: IParser<ReadonlyArray<string>>,
     private readonly imagesParser: IParser<ReadonlyArray<string>>,
-    private readonly videoParser: IParser<ReadonlyArray<string>>,
+    private readonly videosParser: IParser<ReadonlyArray<string>>,
     private readonly asyncStorage: IAsyncStorage<string, string>
   ) {}
 
@@ -48,7 +63,7 @@ export class PageMetaParser implements IParser<IPageMeta> {
       this.tagsParser.parse(rawHTML),
       this.seeAlsoParser.parse(rawHTML),
       this.imagesParser.parse(rawHTML),
-      this.videoParser.parse(rawHTML),
+      this.videosParser.parse(rawHTML),
     ]);
 
     const contentId = this.namer.compute(title);
@@ -72,4 +87,19 @@ export class PageMetaParser implements IParser<IPageMeta> {
   }
 }
 
-throw new Error("describe provide here");
+provide(PageMetaParser, [
+  NameHasher,
+  TitleParser,
+  ContentParser,
+  AuthorNicknameParser,
+  DateCreatedParser,
+  NumberOfViewsParser,
+  ReadingTimeMinutesParser,
+  SourceParser,
+  RatingParser,
+  TagsParser,
+  SeeAlsoParser,
+  ImagesParser,
+  VideosParser,
+  ContentSaver,
+]);
