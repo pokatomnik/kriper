7d682ab7554ad5f42318212fdc89bf5aec6d47f5
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2022-12-04 23:30:20 +0300

    Fix PathConfiguration DI

--- indexer/src/services/configuration/PathConfiguration.ts
+++ indexer/src/services/configuration/PathConfiguration.ts
@@ -1,5 +1,8 @@
 import type { IPathConfiguration } from "./IPathConfiguration.ts";
+import { provide } from "https://deno.land/x/microdi@v0.0.3/Provider.ts";
 
 export class PathConfiguration implements IPathConfiguration {
   public readonly outputPath = "./out";
 }
+
+provide(PathConfiguration, []);
