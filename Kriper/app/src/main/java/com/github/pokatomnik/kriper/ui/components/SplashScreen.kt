8dec5e032075d63944bb3dde7899444ebf91b52a
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2023-01-11 15:51:13 +0300

    Add image for splash screen

--- Kriper/app/src/main/java/com/github/pokatomnik/kriper/ui/components/SplashScreen.kt
+++ Kriper/app/src/main/java/com/github/pokatomnik/kriper/ui/components/SplashScreen.kt
@@ -1,17 +1,35 @@
 package com.github.pokatomnik.kriper.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.github.pokatomnik.kriper.R
 
 @Composable
 fun SplashScreen() {
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
+        color = MaterialTheme.colors.background,
     ) {
-
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = R.drawable.splash_background),
+                contentDescription = "Kriper Logo",
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
