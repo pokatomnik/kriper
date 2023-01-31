896e59559c45f4338040f8269f25631836ecba6b
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2023-01-31 18:50:42 +0300

    Add loading text for video

--- Kriper/app/src/main/java/com/github/pokatomnik/kriper/screens/video/Video.kt
+++ Kriper/app/src/main/java/com/github/pokatomnik/kriper/screens/video/Video.kt
@@ -3,12 +3,16 @@ package com.github.pokatomnik.kriper.screens.video
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.webkit.WebView
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clipToBounds
 import com.github.pokatomnik.kriper.ui.components.PageContainer
 import com.google.accompanist.web.AccompanistWebViewClient
@@ -16,10 +20,16 @@ import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
 
 class KriperWebViewClient(
-    private val onNavigate: (view: WebView?, url: String?, favicon: Bitmap?) -> Unit
+    private val onNavigate: (url: String?) -> Unit,
+    private val onPageFinished: () -> Unit
 ) : AccompanistWebViewClient() {
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+        onPageFinished()
+    }
+
     override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-        onNavigate(view, url, favicon)
+        onNavigate(url)
         super.onPageStarted(view, url, favicon)
     }
 }
@@ -27,24 +37,38 @@ class KriperWebViewClient(
 @SuppressLint("SetJavaScriptEnabled", "WebViewApiAvailability")
 @Composable
 fun VideoInternal(videoURL: String, onNavigate: () -> Unit) {
+    val loadingState = remember { mutableStateOf(true) }
     val state = rememberWebViewState(url = videoURL)
     val client = remember(videoURL) {
-        KriperWebViewClient { _, url, _ ->
-            if (url != videoURL) {
-                onNavigate()
-            }
-        }
+        KriperWebViewClient(
+            onNavigate = { url ->
+                if (url != videoURL) {
+                    onNavigate()
+                }
+            },
+            onPageFinished = { loadingState.value = false }
+        )
     }
 
     PageContainer {
-        WebView(
-            state = state,
-            modifier = Modifier
-                .fillMaxSize()
-                .clipToBounds(),
-            onCreated = { it.settings.javaScriptEnabled = true },
-            client = client
-        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Загрузка...",
+                modifier = Modifier.alpha(if (loadingState.value) 1f else 0f)
+            )
+            WebView(
+                state = state,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clipToBounds()
+                    .alpha(if (loadingState.value) 0f else 1f),
+                onCreated = { it.settings.javaScriptEnabled = true },
+                client = client
+            )
+        }
     }
 }
 
