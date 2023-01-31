43734e2c0ac540cc3e3386d020562fdb762fad68
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2023-01-31 17:29:16 +0300

    Get back video controls

--- Kriper/app/src/main/java/com/github/pokatomnik/kriper/screens/video/Video.kt
+++ Kriper/app/src/main/java/com/github/pokatomnik/kriper/screens/video/Video.kt
@@ -1,33 +1,60 @@
 package com.github.pokatomnik.kriper.screens.video
 
 import android.annotation.SuppressLint
-import android.net.Uri
+import android.graphics.Bitmap
+import android.webkit.WebView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import com.github.pokatomnik.kriper.ui.components.PageContainer
+import com.google.accompanist.web.AccompanistWebViewClient
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
 
+class KriperWebViewClient(
+    private val onNavigate: (view: WebView?, url: String?, favicon: Bitmap?) -> Unit
+) : AccompanistWebViewClient() {
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        onNavigate(view, url, favicon)
+        super.onPageStarted(view, url, favicon)
+    }
+}
+
 @SuppressLint("SetJavaScriptEnabled", "WebViewApiAvailability")
 @Composable
-fun Video(videoURL: String) {
-    val uriWithNoControls = remember(videoURL) {
-        val originalUri = Uri.parse(videoURL)
-        originalUri.buildUpon().appendQueryParameter("controls", "0").build().toString()
+fun VideoInternal(videoURL: String, onNavigate: () -> Unit) {
+    val state = rememberWebViewState(url = videoURL)
+    val client = remember(videoURL) {
+        KriperWebViewClient { _, url, _ ->
+            if (url != videoURL) {
+                onNavigate()
+            }
+        }
     }
-    val state = rememberWebViewState(url = uriWithNoControls)
+
     PageContainer {
         WebView(
             state = state,
             modifier = Modifier
                 .fillMaxSize()
                 .clipToBounds(),
-            onCreated = {
-                it.settings.javaScriptEnabled = true
-            },
+            onCreated = { it.settings.javaScriptEnabled = true },
+            client = client
+        )
+    }
+}
+
+@Composable
+fun Video(videoURL: String) {
+    val videoKeyState = remember { mutableStateOf(0) }
+    key(videoKeyState.value) {
+        VideoInternal(
+            videoURL = videoURL,
+            onNavigate = { ++videoKeyState.value }
         )
     }
 }
