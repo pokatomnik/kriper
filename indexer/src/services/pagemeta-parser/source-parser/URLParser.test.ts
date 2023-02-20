ff43c286eef20ab6d8013f11567606a7f99ab015
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2023-02-20 18:26:06 +0300

    Fix parsing incorrect story source

---
+++ indexer/src/services/pagemeta-parser/source-parser/URLParser.test.ts
@@ -0,0 +1,26 @@
+import { getURL } from "./URLParser.ts";
+import { assertEquals } from "testing";
+
+Deno.test("getURL - test correct URL", () => {
+  const rawURL = "https://example.com";
+  const url = getURL(rawURL);
+  assertEquals(url, rawURL);
+});
+
+Deno.test("getURL - only domain", () => {
+  const rawURL = "www.example.com";
+  const url = getURL(rawURL);
+  assertEquals(url, "http://www.example.com");
+});
+
+Deno.test("getURL - javascript links", () => {
+  const rawURL = "javascript:console.log('no way!')";
+  const url = getURL(rawURL);
+  assertEquals(url, null);
+});
+
+Deno.test("getURL - weird text instead of URL", () => {
+  const rawURL = "Страшные Истории ☠ Lucifera ☠";
+  const url = getURL(rawURL);
+  assertEquals(url, null);
+});
