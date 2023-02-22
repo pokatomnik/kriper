1b63d2d3074c7b58dc509c114919ebb692e4cc12
Danilian Akhmedzianov <pokatomnik@yandex.ru>
2023-02-22 13:43:43 +0300

    Replace old splash screen with a new one (#104)

--- Kriper/app/src/main/java/com/github/pokatomnik/kriper/ui/widgets/SplashScreen.kt
+++ Kriper/app/src/main/java/com/github/pokatomnik/kriper/ui/widgets/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.github.pokatomnik.kriper.ui.widgets
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,11 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import com.github.pokatomnik.kriper.R
 
 @Composable
 fun SplashScreen() {
+    val configuration = LocalConfiguration.current
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background,
@@ -26,7 +29,13 @@ fun SplashScreen() {
         ) {
             Image(
                 modifier = Modifier.fillMaxSize(),
-                painter = painterResource(id = R.drawable.splash_background),
+                painter = painterResource(
+                    id = when (configuration.orientation) {
+                        Configuration.ORIENTATION_LANDSCAPE -> R.drawable.splash_landscape
+                        Configuration.ORIENTATION_PORTRAIT -> R.drawable.splash_portrait
+                        else -> R.drawable.splash_undefined
+                    }
+                ),
                 contentDescription = "Kriper Logo",
                 contentScale = ContentScale.Crop
             )
